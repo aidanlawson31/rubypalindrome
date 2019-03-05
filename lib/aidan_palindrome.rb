c0308547a5475ad29c294beeffdd770a5864f1c9
Aidan <aidanlawson31@gmail.com>
2019-03-05 16:21:20 +1100

    Initialize repository

---
+++ lib/aidan_palindrome.rb
@@ -0,0 +1,6 @@
+require "aidan_palindrome/version"
+
+module AidanPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/aidan_palindrome/version.rb
@@ -0,0 +1,3 @@
+module AidanPalindrome
+  VERSION = "0.1.0"
+end
